d9b0e7a780c1b0fddfed3004f437bc759f5dce63
developervinit <vinit91.dev@gmail.com>
2026-05-06 20:25:28 +0530

    fix: checking for displaying values

--- includes/services/class-zbp-slot-service.php
+++ includes/services/class-zbp-slot-service.php
@@ -198,6 +198,15 @@ class ZBP_Slot_Service {
             $blocks = array();
         }
 
+        echo "<pre>";
+        print_r(array(
+                'stage'              => 'woo_blocks_generated',
+                'product_id'         => (int) $product->get_id(),
+                'booking_form_class' => get_class( $booking_form ),
+                'blocks_count'       => is_array( $blocks ) ? count( $blocks ) : 0,
+            ))
+        echo "</pre>";
+
         $this->debug_log(
             array(
                 'stage'              => 'woo_blocks_generated',
